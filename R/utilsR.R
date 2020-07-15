0312de824002754aaefac35eeccd96c99aa73d89
benjaminguinaudeau <bguinaudeau@gmail.com>
2020-07-15 19:10:29 -0400

    init

---
+++ R/utilsR.R
@@ -0,0 +1,45 @@
+#' get_browser_endpoint
+#' @export
+get_browser_endpoint <- function(debug_port = 9222){
+  jsonlite::fromJSON(rawToChar(httr::GET(glue::glue('http://localhost:{debug_port}/json/version'))[["content"]]))[["webSocketDebuggerUrl"]]
+}
+
+#' launch_chrome_mac
+#' @export
+launch_chrome_mac <- function(debug_port = 9222, data_dir = here::here()){
+  system(glue::glue("/Applications/Google\\\ Chrome.app/Contents/MacOS/Google\\\ Chrome --remote-debugging-port={debug_port} --no-first-run --no-default-browser-check --user-data-dir={data_dir} &"))
+}
+
+#' get_tabs
+#' @export
+get_tabs <- function(debug_port = 9222){
+  jsonlite::fromJSON(rawToChar(httr::GET(glue::glue('http://localhost:{debug_port}/json'))[["content"]]))
+}
+
+#' browser
+#' @export
+browser <- R6::R6Class(
+  "browser",
+  public = list(
+    debug_port = NULL,
+    endpoint = NULL,
+    pages = NULL,
+    initialize = function(debug_port = 9222){
+      self$debug_port = debug_port
+      self$endpoint <- get_browser_endpoint(debug_port = debug_port)
+    },
+    list_tabs = function(){
+      get_tabs(debug_port = self$debug_port) %>%
+        dplyr::filter(type == "page") %>%
+        .[c("title", "url")]
+    },
+    execute = function(title = "", script = ""){
+      title %>%
+        purrr::map(~{
+          out <- py$page(endpoint = self$endpoint, title = .x, script = script)
+          if(!"source" %in% names(out)) "Page not found, verify endpoint and tab title"
+          out$source
+        })
+    }
+  )
+)
---
+++ page.py
@@ -0,0 +1,25 @@
+import asyncio
+import pyppeteer
+
+class page:
+  def __init__(self, endpoint, title, script):
+    self.endpoint = endpoint
+    self.title = title
+    self.script = script
+    
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(self.execute())
+    
+  async def execute(self):
+      browser = await pyppeteer.connect({'browserWSEndpoint': self.endpoint})
+      pages = []
+      for target in browser.targets():
+          if target.type == 'page':
+              page = await target.page()
+              if page:
+                  title = await page.title()
+                  if title == self.title:
+                    try:
+                      self.source = await page.evaluate(self.script)
+                    except:
+                      self.source = sys.exc_info()[0]
